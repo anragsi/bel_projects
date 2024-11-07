3d21eddf3691e7bb6de735e210112ae1a48c4af6
anragsi <a.ranz@gsi.de>
2024-11-07 15:36:28 +0100

    Rebuilding blinky

--- modules/blinky/sim/blinky_testbench.vhd
+++ modules/blinky/sim/blinky_testbench.vhd
@@ -21,9 +21,13 @@ architecture blinky_testbench_architecture of blinky_testbench is
   constant c_reset_time   : time  := 200 ns;
   constant c_clock_cycle  : time  := 16 ns;
   constant c_light_time   : time  := 32 ns;
+  constant c_sim_time     : time  := 20 ms;
+
+  constant c_test_write   : integer  := 1;
+  constant c_test_read    : integer  := 1;
 
   -- Other constants
-  constant c_reg_all_zero                : std_logic_vector(31 downto 0) := x"00000000";
+  constant c_reg_all_zero                : std_logic_vector(31 downto 0) := ( others => '0');
   constant c_cyc_on                      : std_logic := '1';
   constant c_cyc_off                     : std_logic := '0';
   constant c_str_on                      : std_logic := '1';
@@ -31,36 +35,51 @@ architecture blinky_testbench_architecture of blinky_testbench is
   constant c_we_on                       : std_logic := '1';
   constant c_we_off                      : std_logic := '0';
 
-  constant c_blinky_adr   : std_logic_vector(31 downto 0) := x"00000000";
-  constant c_blinky_on    : std_logic_vector(31 downto 0) := x"00000001";
-  constant c_blinky_off   : std_logic_vector(31 downto 0) := x"00000000";
-  constant c_blinky_B_on  : std_logic_vector(31 downto 0) := x"00000011";
-  constant c_blinky_C_on  : std_logic_vector(31 downto 0) := x"00000101";
+  constant c_blinky_on    : std_logic_vector(31 downto 0) := ( 0 => '1', others => '0');
+  --constant c_blinky_on    : std_logic_vector(31 downto 0) := ( others => '1');
+  constant c_blinky_off   : std_logic_vector(31 downto 0) := ( others => '0');
+  constant c_blinky_B_on  : std_logic_vector(31 downto 0) := ( 0 => '1', 1 => '1', 2 => '1', 3 => '1', others => '0');
+  constant c_blinky_C_on  : std_logic_vector(31 downto 0) := ( 0 => '1', 1 => '1', 2 => '0', 3 => '1', others => '0');
   
   -- Basic device signals
   signal s_clk        : std_ulogic := '0';
   signal s_rst_n      : std_ulogic := '1';
   
   -- Wishbone connections
-  signal s_wb_master_in  : t_wishbone_slave_out; -- equal to t_wishbone_master_in
-  signal s_wb_master_out : t_wishbone_slave_in; -- which is equal to t_wishbone_master_out
+  signal s_wb_master_in  : t_wishbone_slave_out;  -- equal to t_wishbone_master_in
+  signal s_wb_master_out : t_wishbone_slave_in;   -- equal to t_wishbone_master_out
 
   -- blinky specific signals
   signal s_led_state : std_ulogic := '0';
   
   -- Function wb_stim -> Helper function to create a human-readable testbench
   function wb_stim(cyc : std_logic; stb : std_logic; we : std_logic;
-                   adr : t_wishbone_address; dat : t_wishbone_data) return t_wishbone_slave_in is
+                   dat : t_wishbone_data) return t_wishbone_slave_in is
   variable v_setup : t_wishbone_slave_in;
   begin
     v_setup.cyc := cyc;
     v_setup.stb := stb;
     v_setup.we  := we;
-    v_setup.adr := adr;
+    v_setup.adr := (others => '0'); -- Don't care
     v_setup.dat := dat;
     v_setup.sel := (others => '0'); -- Don't care
     return v_setup;
   end function wb_stim;
+
+function to_integer( s : std_logic ) return natural is
+begin
+      if s = '1' then
+      return 1;
+   else
+      return 0;
+   end if;
+end function;
+
+  --! @brief Return a intrepresentation of a logic value
+function to_logic_to_int(x : std_logic) return natural is
+  begin
+    return to_integer(x);
+  end function;
 
 
   component blinky is
@@ -99,39 +118,50 @@ architecture blinky_testbench_architecture of blinky_testbench is
         wait for c_clock_cycle/2;
       end process;
 
-    -- Reset controller
-    --p_reset : process
-    --begin
-    --    s_rst_n <= '0';
-    --    wait for c_reset_time;
-    --    s_rst_n <= '1';
-    --end process;
+    --Reset controller
+    p_reset : process
+    begin
+       s_rst_n <= '0';
+       wait for c_reset_time;
+       s_rst_n <= '1';
+       wait for c_sim_time;
+    end process;
 
 
     p_test: process
       begin
         -- RESET active
-        wait until rising_edge(s_clk);
-        s_rst_n <= '0';
-        s_wb_master_out  <= wb_stim(c_cyc_off, c_str_off, c_we_off, c_reg_all_zero, c_reg_all_zero);
-        wait for c_reset_time;
-        s_rst_n <= '1';
+        --
+        s_wb_master_out  <= wb_stim(c_cyc_off, c_str_off, c_we_off, c_blinky_off);
+        wait until rising_edge(s_rst_n);
         -- RESET inactive
+        --
         -- test SINGLE WRITE
-        wait until rising_edge(s_clk);
-        s_wb_master_out  <= wb_stim(c_cyc_on, c_str_on, c_we_on, c_blinky_adr, c_blinky_on);
-        wait for c_clock_cycle/ 2;
-        s_wb_master_out  <= wb_stim(c_cyc_off, c_str_off, c_we_off, c_blinky_adr, c_blinky_off);
+        --
+        for i in 1 to c_test_write loop
+          wait until rising_edge(s_clk);
+          s_wb_master_out  <= wb_stim(c_cyc_on, c_str_on, c_we_on, c_blinky_on);
+          report ("c_blinky_on(0) is: ");
+          report std_logic'image(c_blinky_on(0));
+          wait for c_clock_cycle;
+          s_wb_master_out  <= wb_stim(c_cyc_off, c_str_off, c_we_off,c_blinky_off);
+          wait for 160 ns;
+        end loop;
+        --
         -- test SINGLE WRITE END
-        wait for 160 ns;
+        --
         -- test SINGLE READ
-        wait until rising_edge(s_clk);
-        s_wb_master_out  <= wb_stim(c_cyc_on, c_str_on, c_we_off, c_blinky_adr, c_reg_all_zero);
-        wait for c_clock_cycle/ 2;
-        s_wb_master_out  <= wb_stim(c_cyc_off, c_str_off, c_we_off, c_blinky_adr, c_reg_all_zero);
+        --
+        for i in 1 to c_test_read loop
+          wait until rising_edge(s_clk);
+          s_wb_master_out  <= wb_stim(c_cyc_on, c_str_on, c_we_off, c_reg_all_zero);
+          wait for c_clock_cycle;
+          s_wb_master_out  <= wb_stim(c_cyc_off, c_str_off, c_we_off, c_reg_all_zero);
+          wait for 160 ns;
+        end loop;
+        --
         -- test SINGLE READ END
-        wait for 160 ns;
-
+        --
       end process;
 
 end architecture;
--- modules/blinky/src/blinky.vhd
+++ modules/blinky/src/blinky.vhd
@@ -12,7 +12,7 @@ entity blinky is
     
     -- these two sys signal come from SysCon
     s_clk_sys_i       : in std_ulogic;
-    s_rst_sys_n_i       : in std_ulogic;
+    s_rst_sys_n_i     : in std_ulogic;
 
 
     t_wb_out          : out t_wishbone_slave_out;
@@ -41,82 +41,109 @@ end entity;
 
 architecture blinky_arch of blinky is
 
-  --signal s_rst_sys_n        : std_logic := '0';
-  signal s_led_state        : std_logic := '0';
-  signal s_stall_state      : std_logic := '0';
+    --signal s_rst_sys_n        : std_logic := '0';
+    signal s_led_state        : std_logic := '0';
+    signal s_stall_state      : std_logic := '0';
 
-  --signal s_led_freq_ctl     : std_logic := '0';
-  --signal s_count            : integer   :=  1;
-  --signal s_compare_to       : integer   :=  1;
+    signal s_state_machine_vector : std_logic_vector(3 downto 0) := "0000";
 
-  --signal  s_blinky_mode_v   : std_logic_vector(2 downto 0);
+    --signal s_led_freq_ctl     : std_logic := '0';
+    --signal s_count            : integer   :=  1;
+    --signal s_compare_to       : integer   :=  1;
 
-  --constant s_blinky_mode_A  : std_logic_vector(2 downto 0) := "000";
-  --constant s_blinky_mode_B  : std_logic_vector(2 downto 0) := "001";
-  --constant s_blinky_mode_C  : std_logic_vector(2 downto 0) := "100";
+    --signal  s_blinky_mode_v   : std_logic_vector(2 downto 0);
+
+    --constant s_blinky_mode_A  : std_logic_vector(2 downto 0) := "000";
+    --constant s_blinky_mode_B  : std_logic_vector(2 downto 0) := "001";
+    --constant s_blinky_mode_C  : std_logic_vector(2 downto 0) := "100";
+
+    -- all vectors are downto-range positions are 3210
+    constant mode_write   : std_logic_vector(3 downto 0) := "1111";
+    constant mode_read    : std_logic_vector(3 downto 0) := "0111";
+
+    constant mode_reset_0 : std_logic_vector(3 downto 0) := "0000";
+    constant mode_reset_1 : std_logic_vector(3 downto 0) := "0010";
+    constant mode_reset_2 : std_logic_vector(3 downto 0) := "0100";
+    constant mode_reset_3 : std_logic_vector(3 downto 0) := "0110";
+    constant mode_reset_4 : std_logic_vector(3 downto 0) := "1000";
+    constant mode_reset_5 : std_logic_vector(3 downto 0) := "1010";
+    constant mode_reset_6 : std_logic_vector(3 downto 0) := "1110";
 
 begin
   
-  --  for now no errors, no stalling
-  t_wb_out.err    <= '0';
-  t_wb_out.stall  <= s_stall_state;
-  t_wb_out.rty    <= '0';
+    --  for now no errors, no stalling
+    t_wb_out.err    <= '0';
+    t_wb_out.stall  <= s_stall_state;
+    t_wb_out.rty    <= '0';
 
-  t_wb_out.ack <= t_wb_in.stb and t_wb_in.cyc;
+    t_wb_out.ack <= t_wb_in.stb and t_wb_in.cyc;
 
-  s_led_o <= s_led_state;
-  --s_led_o <= s_led_state and s_led_freq_ctl;
+    s_led_o <= s_led_state;
+    --s_led_o <= s_led_state and s_led_freq_ctl;
 
-  -- single read write cycle
-  p_wb_read_write: process(s_clk_sys_i, s_rst_sys_n_i)
 
-  begin
+    -- fill the stupid pseudo state machine
+    s_state_machine_vector(0) <= s_rst_sys_n_i;
+    s_state_machine_vector(1) <= t_wb_in.cyc;
+    s_state_machine_vector(2) <= t_wb_in.stb;
+    s_state_machine_vector(3) <= t_wb_in.we;
 
-  if rising_edge(s_clk_sys_i) then
+    -- single read write cycle
+    p_wb_read_write: process(s_clk_sys_i, s_rst_sys_n_i)
 
-    -- RESET
-    -- reset is active low
-    if (s_rst_sys_n_i = '0') then
-      if rising_edge(s_clk_sys_i) then
-      -- all wishbone interfaces must initialize on the rising clock 
-      -- after reset was called
+    begin
 
-      -- RESET
-      t_wb_out.dat    <= (others => '0');
+        if rising_edge(s_clk_sys_i) then
 
-      s_led_state     <= '0';
-      s_stall_state   <= '0';
+            case s_state_machine_vector is
 
-      --s_blinky_mode_v   <= (others => '0');
-      --s_led_freq_ctl  <= '1';
-      --s_count         <=  1;
-      --s_compare_to    <=  15; --15000
-      end if; --rising_edge(s_clk_sys_i)
+                when mode_reset_0 | mode_reset_1 | mode_reset_2 | mode_reset_3 | mode_reset_4 | mode_reset_5 | mode_reset_6 =>
 
-      -- END t_wb_in.stb = '1' and t_wb_in.cyc = '1' and t_wb_in.we = '1'
+                    -- RESET
 
-    -- WRITE
-    elsif t_wb_in.stb = '1' and t_wb_in.cyc = '1' and t_wb_in.we = '1' then
+                    t_wb_out.dat    <= (others => '0');
 
-      s_led_state        <= t_wb_in.dat(0);
+                    s_led_state     <= '0';
+                    s_stall_state   <= '0';
+
+                    report("reset");
+                    --report std_logic'image(s_rst_sys_n_i);
+                    --report std_logic'image(s_state_machine_vector(0));
+                    --report std_logic'image(mode_reset_0(0));
+
+                    --s_blinky_mode_v   <= (others => '0');
+                    --s_led_freq_ctl  <= '1';
+                    --s_count         <=  1;
+                    --s_compare_to    <=  15; --15000
+                    --end if; --rising_edge(s_clk_sys_i)
+
+                when mode_write =>
+
+                    s_led_state <= t_wb_in.dat(0);
+                    report("inside write: 1111");
+                    report std_logic'image(t_wb_in.dat(0));
+                    report std_logic'image(t_wb_in.dat(31));
       
-      -- END WRITE
-      -- END t_wb_in.stb = '1' and t_wb_in.cyc = '1' and t_wb_in.we = '1'
-  
-    -- READ
-    elsif t_wb_in.stb = '1' and t_wb_in.cyc = '1' and t_wb_in.we = '0' then
-      
-      t_wb_out.dat(0) <= s_led_state;
-      
-      -- END READ
-      -- END t_wb_in.stb = '1' and t_wb_in.cyc = '1' and t_wb_in.we = '0'
+                -- END WRITE
 
+                when mode_read =>
 
-    end if;
+                    t_wb_out.dat(0) <= s_led_state;
+                    report("inside read: 0111");
+                    report std_logic'image(t_wb_out.dat(0));
+                    report std_logic'image(t_wb_in.dat(31));
+          
+                -- END READ
 
-  -- END rising_edge(s_clk_sys_i)
-  end if;
+                when others => 
+                    --report("Fuck VHDL");
+                    --report std_logic'image(t_wb_in.cyc);
+                    --report std_logic'image(t_wb_in.dat(0));
 
-  end process;
+            end case;
+
+        end if;
+
+    end process;
             
 end blinky_arch;
